f6ce78304b40417251879acb58884e87f81b0c45
danish45007 <myemail>
2020-05-25 22:55:33 +0530

    First

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
-var app = require('electron').app
+'use strict';
 
-var BrowserWindow = require('electron').BrowserWindow
+var app = require('electron').app;
+var BrowserWindow = require('electron').BrowserWindow;
+var mainWindow = null;
+var ipc = require('electron').ipcMain;
+var os = require('os');
+var {dialog} = require('electron');
 
-var mainWindow = null
+ipc.on('close-main-window', function() {
+    app.quit();
+});
 
-app.on('ready', function () {
+app.on('ready', function() {
     mainWindow = new BrowserWindow({
         resizable: true,
-        height:600,
+        height: 600,
         width: 800,
-        webPreferences: {
-            nodeIntergation:true
+        webPreferences:{
+          nodeIntegration:true
         }
-    })
+    });
 
-    mainWindow.loadURL("file://"+ __dirname + '/main.html')
+mainWindow.loadURL('file://' + __dirname + '/main.html');
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+});
 
-    // Close event to release memory
-    mainWindow.on('closed', function(){
-        mainWindow = null
-    })
-  })
+
+
+ipc.on('open-file-dialog-for-file', function (event) {
+    console.log("button pressed")
+    console.log(event)
+    if(os.platform() === 'linux' || os.platform() === 'win32'){
+
+        dialog.showOpenDialog(null, {
+            properties: ['openFile']
+          }).then(result => {
+            console.log(result.filePaths)
+            event.sender.send("selected-file",result.filePaths[0])
+          }).catch(err => {
+            console.log(err)
+          })
+   } else {
+       dialog.showOpenDialog({
+           properties: ['openFile', 'openDirectory']
+       }, function (files) {
+           if (files) event.sender.send('selected-file', files[0]);
+       });
+   }});
+
